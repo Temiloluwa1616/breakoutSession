e7caae331b738cbfd684fff1990292c7219df02e
Temiloluwa1616 <sulaimantemiloluwa24@gmail.com>
2023-02-17 13:50:33 +0000

    pushed breakout assignment

---
+++ assignment.js
@@ -0,0 +1,42 @@
+function tellfortune(X,Y,Z,N){
+  let result= "You will be an " + X + " In" + Y + "and married to " + Z + "with " + N + " Kids. "
+ return result
+}
+console.log(tellfortune("accountant"," lekki "," tosin ", 12))
+
+function HW(X){
+  let result= "Hello " + X + " how are you doing today." 
+    return result }
+console.log(HW("Temiloluwa"))
+
+function calculatepuppyage(humanage){
+  return result = "puppy age is " + humanage*7
+}
+console.log(calculatepuppyage(7))
+
+function calculatesupply(x,y){
+  let additionofsupply= x+y
+  let result = "you will need " + additionofsupply + " to last you until the ripe old age of " + x
+ return result }
+console.log(calculatesupply(49,40))
+
+function calcircumference(radius){
+  let formula= 2*3.142*radius
+  let circumference ="The circumference of the circle is " + formula
+  return circumference
+}
+console.log(calcircumference(4))
+
+ function celsiusToFahrenheit(celsius){
+   let formula= celsius*1.8+32
+   let result= celsius + " celsius is " + formula + " fahrenheit"
+   return result
+ }
+console.log(celsiusToFahrenheit(2))
+
+function fahrenheitToCelsius(fahrenheit){
+   let formula= fahrenheit-32*.5556
+   let result= fahrenheit + " fahrenheit is " + formula + " celsius"
+   return result
+ }
+console.log(fahrenheitToCelsius(4))
